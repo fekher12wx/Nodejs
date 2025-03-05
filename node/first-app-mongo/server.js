881f4b69274d025cc7e90285d9b4e364ec3b6b40
fekher <fekheryahya@gmail.com>
2025-03-05 23:48:58 +0100

    auth with user crud

---
+++ node/first-app-mongo/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+const userRoutes = require("./routes/user.route");
+const AuthRoutes =require('./routes/auth.routes');
+
+app.use(express.json());
+app.use("/users", userRoutes);
+
+app.use('/auth',AuthRoutes);
+
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to server database");
+  })
+  .catch((err) => {
+    console.log("error connecting to database");
+  });
+
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+  console.log("listening on port" + port);
+});
